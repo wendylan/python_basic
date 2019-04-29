aaa9b4dcd58f3f6ffce86532a5251d6fa5d55598
wendy <3063766545@qq.com>
2019-04-29 19:17:44 +0800

    面向对象编程基础

---
+++ hack.py
@@ -0,0 +1,19 @@
+"""
+另一种创建类的方式
+"""
+
+def bar(self, name):
+    self._name = name;
+
+
+def foo(self, course_name):
+    print('%s正在学习%s.' % (self._name, course_name));
+
+def main():
+    Student = type('Student', (object,), dict(__init__=bar, study=foo));
+    stu1 = Student('wendy');
+    stu1.study('Python程序设计');
+
+
+if __name__ == '__main__':
+	main();
